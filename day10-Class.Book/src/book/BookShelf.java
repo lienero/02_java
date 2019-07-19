3df0f230260a1a4dd5ded439a04fa1dfe44e9fba
lienero <lienero@naver.com>
2019-07-19 09:45:37 +0900

    day10

--- day10-Class.Book/src/book/BookShelf.java
+++ day10-Class.Book/src/book/BookShelf.java
@@ -58,23 +58,36 @@ public class BookShelf {
 	//	  폐기할 책이 있다는 의미로 판단하고 삭제로직 진입
 	if (index > -1) {
 	
-	// 3. 폐기 안할 책을 유지할 
-	//	  새 배열을 지금 배열 - 1 크기로 생성
+		// 3. 폐기 안할 책을 유지할 
+		//	  새 배열을 지금 배열 - 1 크기로 생성
 		newBooks = new Book[books.length - 1];
 
-	// 4. 폐기할 인덱스가 배열 중간일 때
-	//  (1) 삭제할 책 앞쪽의 책정보는 같은 인덱스로 복사
-	//	(2) 삭제할 책 뒤쪽의 남는 책정보는 현재 인덱스 - 1 위치로 복사한다.
-		if (index )
-		
-	// 5. 폐기할 인덱스가 배열 끝일 때
-	//	  폐기할 책 인덱스 앞쪽까지만 새 배열에 복사
+		// 4. 폐기할 인덱스가 배열 중간일 때
+		if (index < books.length - 1 ) {
+			//  (1) 삭제할 책 앞쪽의 책정보는 같은 인덱스로 복사
+			for (int idx = 0; idx < index; idx++) {
+				newBooks[idx] = books[idx];
+			}
+			
+		//	(2) 삭제할 책 뒤쪽의 남는 책정보는 현재 인덱스 - 1 위치로 복사한다.
+			for (int idx = index; idx < newBooks.length; idx++) {
+				newBooks[idx] = books[idx + 1];
+			}
+		} else {
+		// 5. 폐기할 인덱스가 배열 끝일 때 ( index == books.length)  
+		//	  폐기할 책 인덱스 앞쪽까지만 새 배열에 복사
+			for (int idx = 0; idx < books.length - 1; idx++ ) {
+				newBooks[idx] = books[idx];
+			}
+		}
+		// 6. 남는 책이 복사된 새 배열을
+		// 	  this.book 에 새로 저장
+		this.books = newBooks;
 	
-	// 6. 남는 책이 복사된 새 배열을
-	// 	  this.book 에 새로 저장
-	this.books = newBooks;
+	} // end outer if 
+
+}
 	
-	}
 	// 책 정보 수정 : void : set(Book book)
 	public void set(Book book) {
 		// 수정할 book 이 books 배열 
@@ -95,6 +108,20 @@ public class BookShelf {
 		return findBook(book);
 	}
 	
+	// 전체 책 목록을 얻기 : getAllBooks()
+	public Book[] getAllBooks() {
+		return this.books;
+	}
+	
+	
+	/**
+	 * 매개변수 전달된 책 정보와
+	 * 일치하는 일련번호를 가진 책(책 배열 : books에 있는)을
+	 * 찾아서 배열 안에 들어있는 책을 리턴
+	 *  
+	 * @param book
+	 * @return
+	 */
 	// 책을 찾는 리턴 메소드
 	Book findBook(Book book) {
 		Book findBook = null;
@@ -122,8 +149,6 @@ public class BookShelf {
 		
 		return index;
 	}	
-	
-	// 전체 책 목록을 얻기
 	
 	
 	
