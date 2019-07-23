3082659713fc33dac88ab9f6c6a3c2a8d648ccc5
lienero <lienero@naver.com>
2019-07-23 09:51:08 +0900

    day13 Student

--- day13-Inheritance.School/src/school/Student.java
+++ day13-Inheritance.School/src/school/Student.java
@@ -1,16 +1,37 @@
 package school;
 
 public class Student extends Person {
-	// 1. 멤버변수
+	// 1. 멤버변수 선언부
+	/** 학생의 전공을 저장하는 멤버변수 */
 	private String major;
 	
-	// 2. 생성자 중복정의
-	public Student(String id, String name, int age) {
-		super(id, name, age);
+	// 2. 생성자 선언부
+	// (1) 기본 생성자
+	public Student() {
+		
+	}
+	// (2) 매개변수 받는 생성자
+	/**
+	 * Student 만의 필드인 major를 초기화하는 필드
+	 * @param major
+	 */
+	public Student(String major) {
+		this.major = major;
 	}
 	
+	/**
+	 * Person 클래스에서 물려받은 필드(id, name, age) 와
+	 * Student 만의 필드인 major 를 모두 초기화하는 생성사
+	 * @param id
+	 * @param name
+	 * @param age
+	 * @param major
+	 */
 	public Student(String id, String name, int age, String major) {
-		this(id, name, age);
+		super(id, name, age);
+//		this(major);
+		// super(), this() 는 생성자 선언 첫줄에 한번만 등장 가능
+		// super() 도 사용하고, this() 도 사용할 순 없다
 		this.major = major;
 	}
 	
@@ -23,10 +44,13 @@ public class Student extends Person {
 		this.major = major;
 	}
 	
-	// 4. toSting() 재정의
+	// 4. 메소드 선언부
+	// (1) toString() 재정의 : 학생에 맞도록
 	@Override
 	public String toString() {
-		return super.toString() + major;
+		String stbStr = String.format(", 전공:%s", major);
+//		return "학생정보[" + super.toString() + stsbStr + "]";
+		return String.format("학생정보[%s%s]", super.toString(), stbStr);
 	}
 
 }
